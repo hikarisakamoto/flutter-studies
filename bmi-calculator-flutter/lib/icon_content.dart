14557116b1f8725e8a91983e2eef7ea752d2bd7e
Hikari Sakamoto <hikari@townsq.com.br>
2020-05-14 23:59:16 -0300

    BMI Calculator initialized

---
+++ bmi-calculator-flutter/lib/icon_content.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+
+class IconContent extends StatelessWidget {
+  IconContent({@required this.iconData, @required this.text});
+
+  static const iconSize = 75.0;
+  static const fontSize = 14.0;
+  static const height = 15.0;
+  static const color = Color(0xFF8D8E98);
+
+  final IconData iconData;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Icon(
+          iconData,
+          size: iconSize,
+        ),
+        SizedBox(
+          height: height,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            color: color,
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -0,0 +1,97 @@
+import 'package:bmi_calculator/reusable_card.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'icon_content.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const inacticeCardColor = Color(0xFF111328);
+const bottomContainerColor = Color(0xFFEB1555);
+
+enum Gender { male, female }
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Gender selectedGender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    child: ReusableCard(
+                      color: selectedGender == Gender.male ? activeCardColor : inacticeCardColor,
+                      cardChild: IconContent(
+                        iconData: FontAwesomeIcons.mars,
+                        text: 'MALE',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    child: ReusableCard(
+                      color: selectedGender == Gender.female ? activeCardColor : inacticeCardColor,
+                      cardChild: IconContent(
+                        iconData: FontAwesomeIcons.venus,
+                        text: 'FEMALE',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(color: activeCardColor),
+          ),
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  color: activeCardColor,
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  color: activeCardColor,
+                ),
+              ),
+            ],
+          )),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
+      ),
+    );
+  }
+}
